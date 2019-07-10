07f2d5503d458f01df18837c521608ac7c272051
UchMan <ucha@clips4sale.com>
2019-07-11 02:00:03 +0400

    change

--- src/scenes/bootScene.ts
+++ src/scenes/bootScene.ts
@@ -7,6 +7,10 @@ export class BootScene extends Phaser.Scene {
     preload(): void {
         this.load.image('wheel', './src/assets/images/wheel.png');
         this.load.image('pin', './src/assets/images/pin.png');
+
+        this.load.audio('sample','./src/sounds/sample.mp3')
+        this.load.audio('bublidona','./src/sounds/bublidona.aac')
+        this.load.audio('tyorini','./src/sounds/tyorini.aac')
     }
 
     update(): void {
