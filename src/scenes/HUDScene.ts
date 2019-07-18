1f941a996a28c02a86c73fcee8139e862070d0c6
uC137 <ucha19871@gmail.com>
2019-07-18 17:04:06 +0400

    change

--- src/scenes/HUDScene.ts
+++ src/scenes/HUDScene.ts
@@ -17,11 +17,28 @@ export class HUDScene extends Phaser.Scene {
             ["WORLD", this.addText(96, 8, `${this.registry.get("world")}`)],
             ["TIME", this.addText(136, 8, `${this.registry.get("time")}`)]
         ]);
+
+        // create events
+        const level = this.scene.get("GameScene");
+        level.events.on("coinsChanged", this.updateCoins, this);
+        level.events.on("livesChanged", this.updateLives, this);
+        level.events.on("scoreChanged", this.updateScore, this);
+
     }
 
     private addText(x: number, y: number, value: string): Phaser.GameObjects.BitmapText {
         return this.add.bitmapText(x, y, "font", value, 8);
     }
 
+    private updateCoins() {
+        this.textElements.get("COINS").setText(`${this.registry.get("coins")}`).setX(80 - 8 * (this.registry.get("coins").toString().length - 1));
+    }
 
+    private updateLives() {
+        this.textElements.get("LIVES").setText(`Lives: ${this.registry.get("lives")}`);
+    }
+
+    private updateScore() {
+        this.textElements.get("SCORE").setText(`${this.registry.get("score")}`).setX(40 - 8 * (this.registry.get("score").toString().length - 1));
+    }
 }
