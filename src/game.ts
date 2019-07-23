4f076e6356057f278c5296d75ee5a219dd1a938a
uC137 <ucha19871@gmail.com>
2019-07-23 18:04:39 +0400

    emptyout

--- src/game.ts
+++ src/game.ts
@@ -2,19 +2,18 @@ import "phaser";
 import {BootScene} from "./scenes/bootScene";
 import {MenuScene} from "./scenes/mainMenuScene";
 import {GameScene} from "./scenes/gameScene";
-import {HUDScene} from "./scenes/HUDScene";
 
 // main game configuration
 const config: GameConfig = {
-    title: "Tate-Tute",
+    title: "Tank",
     version: "1.0",
-    width: 160,
-    height: 144,
-    zoom: 5,
+    width: 1600,
+    height: 1200,
+    zoom: 0.6,
     type: Phaser.AUTO,
     parent: "game",
-    backgroundColor: "#f8f8f8",
-    scene: [BootScene, MenuScene, HUDScene, GameScene],
+    backgroundColor: "#000000",
+    scene: [BootScene, MenuScene, GameScene],
     input: {
         keyboard: true,
         mouse: false,
@@ -24,11 +23,11 @@ const config: GameConfig = {
     physics: {
         default: "arcade",
         arcade: {
-            gravity: {y: 475},
+            gravity: {y: 0},
             debug: false
         }
     },
-    render: {pixelArt: true, antialias: false}
+    render: {pixelArt: false, antialias: true}
 };
 
 // game class
