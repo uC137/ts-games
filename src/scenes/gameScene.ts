6f9f83627d49f0b7de6acd59548b7462552f24ac
uC137 <ucha19871@gmail.com>
2019-07-23 19:46:54 +0400

    change

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,14 +1,25 @@
 export class GameScene extends Phaser.Scene {
+    private map: Phaser.Tilemaps.Tilemap;
+    private tileset: Phaser.Tilemaps.Tileset;
+    private layer: Phaser.Tilemaps.StaticTilemapLayer;
+
+
+    private enemies: Phaser.GameObjects.Group;
+    private obstacles: Phaser.GameObjects.Group;
 
     constructor() {
         super({key: "GameScene"});
     }
 
-    init(): void {
-        console.log("game Scene");
-    }
+    init(): void {}
 
     create(): void{
+        // create tilemap from tiled JSON
+        this.map = this.make.tilemap({ key: "map" });
+
+        this.tileset = this.map.addTilesetImage("tiles");
+        this.layer = this.map.createStaticLayer("tileLayer", this.tileset, 0, 0);
+        this.layer.setCollisionByProperty({ collide: true });
 
     }
 
