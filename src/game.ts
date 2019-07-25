3089645277ef783c0b3baa6ca24953f838f4b729
uC137 <ucha19871@gmail.com>
2019-07-25 18:39:38 +0400

    change

--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,7 @@ import {GameScene} from "./scenes/gameScene";
 
 // main game configuration
 const config: GameConfig = {
-    title: "Mario",
+    title: "Adventurer",
     version: "1.0",
     width: 300,
     height: 250,
