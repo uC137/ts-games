3e1582670db059d1db13fa2f45c34a9e2696c52b
uC137 <ucha19871@gmail.com>
2019-07-25 20:12:44 +0400

    change

--- src/scenes/mainMenuScene.ts
+++ src/scenes/mainMenuScene.ts
@@ -1,4 +1,6 @@
 export class MainMenuScene extends Phaser.Scene {
+    private startKey: Phaser.Input.Keyboard.Key;
+    private bitmapTexts: Phaser.GameObjects.BitmapText[] = [];
 
     constructor() {
         super({key: "MainMenuScene"});
@@ -6,16 +8,24 @@ export class MainMenuScene extends Phaser.Scene {
 
 
     init(): void {
-
+        this.startKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        this.startKey.isDown = false;
     }
 
     create(): void {
+        this.bitmapTexts.push(
+            this.add.bitmapText(this.sys.canvas.width / 2 - 120,this.sys.canvas.height / 2,"font","PRESS ENTER TO PLAY",15),
+            this.add.bitmapText(this.sys.canvas.width / 2 - 220,this.sys.canvas.height / 2 - 100,"font","ADVENTURER",50),
+        );
 
     }
 
 
     update(): void {
-
+        if (this.startKey.isDown) {
+            console.log("start");
+            //this.scene.start("GameScene");
+        }
     }
 
 }
